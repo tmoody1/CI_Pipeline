d7ed9bddc59f718ec225312eb2cd7699e844eb5e
Thomas Moody <tmoody01@hotmail.com>
2018-12-17 17:08:05 +0000

    Think I have job definitions working now, have overridden cmd and need to delete container/volumes/start from scratch

--- jenkins/security.groovy
+++ jenkins/security.groovy
@@ -5,9 +5,13 @@ import hudson.security.*
 import jenkins.security.s2m.AdminWhitelistRule
 
 def instance = Jenkins.getInstance()
-def env = System.getenv()
-def user = env['user']
-def pass = env['password']
+//def env = System.getenv()
+File infile = new File("/tmp/secrets.txt")
+String user = readLine( infile, 1 ).split('=')[1]
+String pass = readLine( infile, 2 ).split('=')[1]
+
+//def user = env['user']
+//def pass = env['password']
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount(user, pass)
