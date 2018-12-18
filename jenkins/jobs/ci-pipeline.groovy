c98cc635e985baaa8e9c9822614a100298a25f73
Thomas Moody <tmoody01@hotmail.com>
2018-12-18 15:59:39 +0000

    Working project

---
+++ jenkins/jobs/ci-pipeline.groovy
@@ -0,0 +1,13 @@
+node('master'){
+    stage('pull code'){
+        git 'https://github.com/scrappy1987/tracker-docker.git'
+    }
+    stage('build project'){
+        sh 'mvn clean package docker:build'
+    }
+    stage('run project'){
+        sh 'docker run --name trainer-mysql -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=trainer -e MYSQL_USER=trainer_user -e MYSQL_PASSWORD=trainer_pass -d mysql:5.6'
+        sleep(30)
+        sh 'docker run -p 9090:8080 --name trainer-app --link trainer-mysql:mysql -d trainer/trainer-tracker'
+    }
+}
--- jenkins/jobs/test.groovy
+++
@@ -1,5 +0,0 @@
-node('master'){
-    stage('test'){
-        print("ran")
-    }
-}
