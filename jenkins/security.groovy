8bbd289f693bf5feaf01fd45e1274f873e2e17bb
Thomas Moody <tmoody01@hotmail.com>
2018-12-18 10:24:29 +0000

    user is created correctly, just need a way of calling job builder after setup

--- jenkins/security.groovy
+++ jenkins/security.groovy
@@ -5,16 +5,11 @@ import hudson.security.*
 import jenkins.security.s2m.AdminWhitelistRule
 
 def instance = Jenkins.getInstance()
-//def env = System.getenv()
 File infile = new File("/tmp/secrets.txt")
 String user = infile.grep(~/^user.*/)
-user = user.tokenize('=')[1]
+user = user.tokenize('=')[1].replace("]","")
 String pass = infile.grep(~/^password.*/)
-pass = pass.tokenize('=')[1]
-//def user = env['user']
-//def pass = env['password']
-print("The user is"+ user)
-print("the password is"+ pass)
+pass = pass.tokenize('=')[1].replace("]","")
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount(user, pass)
 instance.setSecurityRealm(hudsonRealm)
